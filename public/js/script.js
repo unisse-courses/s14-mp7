7ab463847111770071205668c863c4c7603cf396
remangelo <59946933+remangelo@users.noreply.github.com>
2020-05-13 19:06:16 +0800

    Revert "availability logic"
    This reverts commit 11b336d9620ade373a09de3c33989e7b148b4385.

--- public/js/script.js
+++ public/js/script.js
@@ -121,133 +121,68 @@ $(document).ready(function() {
         if(valid === true){
             var villageTheme = $('#villageTheme').val();
             var pax =  parseInt($('#adultNum').val()) + parseInt($('#childNum').val());
-            var checkInDate = new Date($('#checkIn').val());
-            var checkOutDate = new Date($('#checkOut').val());
-            checkInDate.setHours(0);
-            checkOutDate.setHours(0);
+            var checkInDate = $('#checkIn').val();
+            var checkOutDate = $('#checkOut').val();
             $.post('/availabilities', { villageTheme: villageTheme, pax: pax, checkIn: checkInDate, checkOut: checkOutDate}, function(data, status) {
-            
+                
                 //console.log(data);
                 
                 var villaList = $('#villaList');
                 villaList.empty();
                 
                 if(data.length > 0){
-                    try
-                    {
-                    //getting reservations to show list of availabilities
-                    $.get('/reserveavailabilities').then(function(response) {
-                        var reservationList = response;
-                        console.log(reservationList);
-                        
-                            //show all available villas
-
-                            var vipAvailable = 1;
-                            var deluxeAvailable = 1;
-                            var suiteAvailable = 1;
-                            var premierAvailable = 1;
-
-                            //loop through villas data array
-                            for(var index = 0; index < data.length; index++) {
-                                //loop through the reservations, checking for statuses, dates, capacity, villageTheme
-                                for(var index1 = 0; index1 < reservationList.length; index1++){
-                                    
-                                    var cInDate = new Date(reservationList[index1].checkIn);
-                                    var cOutDate = new Date(reservationList[index1].checkOut);
-                                    var status = reservationList[index1].status;
-                                    var vTheme = reservationList[index1].villa.villageTheme;
-                                    var vType = reservationList[index1].villa.villatype;
-                                    var villaListType = data[index].villatype;
-                                    var vcapacity = data[index].capacity;
-                                    
-                                    if((status == "Active") && (cInDate.getTime() == checkInDate.getTime()) && (cOutDate.getTime() == checkOutDate.getTime()) && (vTheme == villageTheme) && (vType == villaListType) ){
-                                    
-                                        if(villaListType == "vip")
-                                            vipAvailable = 0;
-
-                                            else if(villaListType == "deluxe")
-                                                deluxeAvailable = 0;
-
-                                                else if(villaListType == "suite")
-                                                    suiteAvailable = 0;
-
-                                                    else if(villaListType == "premier")
-                                                        premierAvailable = 0;
-                                    }
-                                
-                                }   
-                            }
-                            
-                            if(vipAvailable == 1  && pax <= 2){
-            
-                                element = data[data.findIndex(x => x.villatype === "vip")];
-                                addVillaDiv(element, villaList);
-                            }
-                            if(deluxeAvailable == 1 && pax <= 4){
-                                element = data[data.findIndex(x => x.villatype === "deluxe")];
-                                addVillaDiv(element, villaList);
-                            }
-                            if(suiteAvailable == 1 && pax <= 6){
-                                element = data[data.findIndex(x => x.villatype === "suite")];
-                                addVillaDiv(element, villaList);
-                            }
-                            if(premierAvailable == 1 && (pax > 0) && (pax <= 8)){
-                                element = data[data.findIndex(x => x.villatype === "premier")];
-                                addVillaDiv(element, villaList);
-                            }
+                    data.forEach((item, i) => {
+                        addVillaDiv(item, villaList);
                     });
-                } catch(err){
-                    console.log(err);
-                }
 
                         // reserve a villa
                         $('.reserveNow').click(function() {
-                                
-                            // Get the data from the form
-                            var reserveId = this.id;
-                            var checkIn = $('#checkIn').val();
-                            var checkOut = $('#checkOut').val();
-                            var adultNum = $('#adultNum').val();
-                            var childrenNum = $('#childNum').val();
-                            var priceId = $('#priceId').val();
-                            var status = "Active";
+                            
+                        // Get the data from the form
+                        var reserveId = this.id;
+                        var checkIn = $('#checkIn').val();
+                        var checkOut = $('#checkOut').val();
+                        var adultNum = $('#adultNum').val();
+                        var childrenNum = $('#childNum').val();
+                        var priceId = $('#priceId').val();
+                        var status = "Active";
+                    
+                        var newReservation = {
+                            checkIn: checkIn,
+                            checkOut: checkOut,
+                            adultNum: adultNum,
+                            childrenNum: childrenNum,
+                            villa: reserveId,
+                            priceId: priceId,
+                            status: status
+                        };
+
+                    //  console.log(newReservation);
                         
-                            var newReservation = {
-                                checkIn: checkIn,
-                                checkOut: checkOut,
-                                adultNum: adultNum,
-                                childrenNum: childrenNum,
-                                villa: reserveId,
-                                priceId: priceId,
-                                status: status
-                            };
-
-                        //  console.log(newReservation);
+                        $.post('/reserve', newReservation).then(function(response) {
                             
-                            $.post('/reserve', newReservation).then(function(response) {
-                                
-                                if (response.success == true) {
-                                    window.location.replace(response.url);
-                                }
-                            
-                            }); 
+                            if (response.success == true) {
+                                window.location.replace(response.url);
+                            }
+                        
                         }); 
-            
-                    }
+                    }); 
+        
+                }
 
-                    else{
-                        var show = document.createElement('p');
-                        var containerDiv = document.createElement('div');
-                        $(containerDiv).addClass("container py-3");
-                        $(show).attr({
-                            "class":"warningText",
-                            "id": 'show'
-                        }).text("No available villas on that date.");
-                        containerDiv.append(show);
-                        villaList.append(containerDiv);
-                    }
-            
-                });
+                else{
+                    var show = document.createElement('p');
+                    var containerDiv = document.createElement('div');
+                    $(containerDiv).addClass("container py-3");
+                    $(show).attr({
+                        "class":"warningText",
+                        "id": 'show'
+                    }).text("No available villas on that date.");
+                    containerDiv.append(show);
+                    villaList.append(containerDiv);
+                }
+        
+            });
         } 
     });
   });
--- routers/employeeRouter.js
+++ routers/employeeRouter.js
@@ -6,6 +6,4 @@ const { isLoggedIn, isAdmin } = require('../middlewares/checkAuth');
 // Reservations Report route
 router.get('/reservations', isLoggedIn, isAdmin, reservationController.getAllReservations);
 
-router.get('/reserveavailabilities', isLoggedIn, isAdmin, reservationController.getAvailabilities)
-
 module.exports = router;
